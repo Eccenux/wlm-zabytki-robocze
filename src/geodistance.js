ae2df9d1b8498c490dd2b563ad7bbf383a1d9c5c
eccenux <eccenux+git@gmail.com>
2023-08-16 01:15:54 +0200

    formatting

--- src/geodistance.js
+++ src/geodistance.js
@@ -2,7 +2,15 @@ import { GeoCalc } from "./GeoCalc.js";
 
 /** Formatowanie na metry/km. */
 const format = (distance) => {
-	return distance < 0.5 ? {j:'m', d:(distance*1000).toFixed(10)} : {j:'km', d:(distance).toFixed(8)};
+	if (distance < 0.00000001)	return {j:'nm', d:(distance*1000*1000000).toFixed(6)};
+	if (distance < 0.00001)		return {j:'mm', d:(distance*1000*1000).toFixed(8)};
+	if (distance < 0.01)		return {j:'m',  d:(distance*1000).toFixed(10)};
+	return {j:'km', d:(distance).toFixed(12)};
+};
+const formatDiff = (diff) => {
+	if (diff == 0)	return diff;
+	if (diff <= 2e-4)	return diff.toExponential();
+	return diff;
 };
 
 /**
@@ -16,7 +24,7 @@ const format = (distance) => {
  */
 function dist (opt) {
 	let {latFun, lonFun} = opt;
-	console.log("dystans\tjednostka\tdokładność lat\tdokładność lon");
+	console.log("dystans wg Δ\tjedn.\tΔlat\tΔlon");
 	for(let i = 13; i >= 1; i--) {
 		let diffLat = latFun(i);
 		let diffLon = lonFun(i);
@@ -26,14 +34,14 @@ function dist (opt) {
 		// Distance in km
 		let distance = GeoCalc.distanceWithHaversine(lat1, lon1, lat2, lon2);
 		let {j, d} = format(distance);
-		console.log("%s\t%s\t%f\t%f", d, j, diffLat, diffLon);
+		console.log("%s\t%s\t%s\t%s", d, j, formatDiff(diffLat), formatDiff(diffLon));
 	}
 	console.log("\n\n");
 }
 
 //~środek Polski
-let lat1=52.2556612131415;
-let lon1=19.1697712131415;
+let lat1=52.2556682736455;
+let lon1=19.1697782736455;
 
 dist({latFun:(i)=>1/10**i, lonFun:()=>0});
 dist({latFun:()=>0, lonFun:(i)=>1/10**i});
