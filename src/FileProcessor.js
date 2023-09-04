77e3ec8f84c0f6507b1ede74c579d482932fe13f
eccenux <eccenux+git@gmail.com>
2023-09-05 00:35:25 +0200

    insert many

---
+++ src/FileProcessor.js
@@ -0,0 +1,50 @@
+import fs from 'fs';
+import { promisify } from 'util';
+
+const readFileAsync = promisify(fs.readFile);
+
+export default class FileProcessor {
+	constructor(db) {
+		this.db = db;
+	}
+
+	async processFiles(directoryPath) {
+		const files = await this.readMatchingFiles(directoryPath);
+		let total = 0;
+		for (const file of files) {
+			const items = await this.readJSONFromFile(directoryPath, file);
+			console.log(`${file}: ${items.length}`);
+			total += items.length;
+			for (const item of items) {
+				this.db.insert(item);
+			}
+		}
+		console.log(`Total items: ${total} in files: ${files.length}.`);
+	}
+
+	/** @private Read JSON files list. */
+	async readMatchingFiles(directoryPath) {
+		try {
+			const files = await fs.promises.readdir(directoryPath);
+			return files.filter(file => file.startsWith('lon_') && file.endsWith('.json'));
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	/** @private Read JSON array from file. */
+	async readJSONFromFile(directoryPath, file) {
+		try {
+			const filePath = `${directoryPath}/${file}`;
+			const data = await readFileAsync(filePath, 'utf8');
+			const items = JSON.parse(data);
+			if (!Array.isArray(items)) {
+				throw `Not an array (${typeof items}) in ${file}`;
+			}
+			return items;
+		} catch (error) {
+			console.warn('Reading JSON failed for %s.', file, error);
+			return [];
+		}
+	}
+}
--- src/dbInsert.js
+++ src/dbInsert.js
@@ -1,20 +1,14 @@
+import FileProcessor from "./FileProcessor.js";
 import DbData from "./db/DbData.js";
 
 /**
  * Insert WD data to local DB.
  */
-const data = {
-	"category": "Białowieża zażółć gęślą jaźń",
-	"coord": {
-		"lat": "52.731572",
-		"lon": "23.867764"
-	},
-	"itemLabel": "Puszcza Białowieska",
-	"townLabel": "województwo podlaskie",
-	"image": "http://commons.wikimedia.org/wiki/Special:FilePath/Wisent.jpg",
-	"item": "Q192666",
-	"town": "Q54177"
-};
-
 const db = new DbData();
-db.insert(data);
+// db.insert(data);
+
+// Define the directory where your JSON files are located
+const directoryPath = './output';
+
+const files = new FileProcessor(db);
+files.processFiles(directoryPath);
