b7e51d12209d2e7633ca514a634f12d9f89b46f4
eccenux <eccenux+git@gmail.com>
2023-09-06 23:20:49 +0200

    new columns

--- src/db/find.dupl.sql
+++ src/db/find.dupl.sql
@@ -4,10 +4,20 @@
 select concat(lat_, ':' , lon_, ':20')
 	--, count(*) as cntAll
 	, max(townLabel) as miasto
+	, max(stateLabel) as woj
 	, count(distinct item) as cntQ
-	, array_agg(distinct item) as items
+	, array_to_string(array_agg(item), ';') as Qid
+	, array_to_string(array_agg(inspireIds), ';') as inspireId
+	, array_to_string(array_agg(typeLabels), ';') as typy
+	, array_to_string(array_agg(monumentStatus), ';') as statusy
 from (
-	SELECT cast(lat as NUMERIC(11,6)) as lat_, cast(lon as NUMERIC(11,6)) as lon_, item, townLabel
+	SELECT cast(lat as NUMERIC(11,6)) as lat_, cast(lon as NUMERIC(11,6)) as lon_
+	, item
+	, townLabel
+	, stateLabel
+	, inspireIds
+	, typeLabels
+	, monumentStatus
 	FROM public.wlz_dupl
 ) as t
 group by lat_, lon_
