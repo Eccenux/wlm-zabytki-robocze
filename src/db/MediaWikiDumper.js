d21f353d82a77f68d3693894e12dac8f068750ce
eccenux <eccenux+git@gmail.com>
2023-09-07 01:59:36 +0200

    better tables * refactor: map of column names * mark column as agg.regate

--- src/db/MediaWikiDumper.js
+++ src/db/MediaWikiDumper.js
@@ -6,15 +6,26 @@ import fs from 'fs';
 
 // Define the SQL query
 // const sqlQuery = fs.readFileSync('./src/db/find.dupl.sql');
+const sqlLimit = 10;
+const sqlNameMap = {
+	'latlon': 'lokalizacja',
+	'town': 'miasto',
+	'state': 'woj.',
+	'cnt': 'licznik',
+	'agg_qid': 'Q (duplikat lub złe współrzędne)',
+	'agg_inspireid': 'inspire id',
+	'agg_type': 'typ obiektu',
+	'agg_status': 'status dobra kultury',
+}
 const sqlQuery = `
 select concat(lat_, ':' , lon_) as latlon
-	, max(townLabel) as miasto
-	, max(stateLabel) as woj
-	, count(distinct item) as licznik
-	, array_to_string(array_agg(item ORDER BY item), '; ') as "Q (duplikaty lub złe współrzędne)"
-	, array_to_string(array_agg(inspireIds ORDER BY item), '; ') as "inspire id"
-	, array_to_string(array_agg(typeLabels ORDER BY item), '; ') as typy
-	, array_to_string(array_agg(monumentStatus ORDER BY item), '; ') as statusy
+	, max(townLabel) as town
+	, max(stateLabel) as state
+	, count(distinct item) as cnt
+	, array_to_string(array_agg(item ORDER BY item), '; ') as agg_qid
+	, array_to_string(array_agg(inspireIds ORDER BY item), '; ') as agg_inspireid
+	, array_to_string(array_agg(typeLabels ORDER BY item), '; ') as agg_type
+	, array_to_string(array_agg(monumentStatus ORDER BY item), '; ') as agg_status
 from (
 	SELECT cast(lat as NUMERIC(11,6)) as lat_, cast(lon as NUMERIC(11,6)) as lon_
 	, item
@@ -27,8 +38,8 @@ from (
 ) as t
 group by lat_, lon_
 having count(distinct item) > 2
-order by licznik desc
-limit 50
+order by cnt desc
+limit ${sqlLimit}
 `;
 
 /**
@@ -78,8 +89,17 @@ export default class MediaWikiDumper {
 	formatAsMediaWikiTable(rows) {
 		let table = '{| class="topalign"\n';
 		const headers = Object.keys(rows[0]);
-		table += '! ' + headers.join(' !! ') + '\n';
+		const headerNames = headers.map((h)=>sqlNameMap[h]);
+		table += '! ' + headerNames.join(' !! ') + '\n';
 
+		/**
+		 * TODO: better tables.
+		 * ✅mark column as agg_(regate)
+		 * rowspan = count rows in any agg_
+		 * add non-agg with rowspan
+		 * add 0-row agg without rowspan
+		 * add other agg as rows
+		 */
 		for (const row of rows) {
 			const values = headers
 				.map((header) => {
@@ -87,7 +107,7 @@ export default class MediaWikiDumper {
 						const ll = row[header];
 						return `[https://zabytki.toolforge.org/#!?c=${ll}:20 ${ll}]`;
 					}
-					const isQ = (header.startsWith('Q '));
+					const isQ = (header === 'agg_qid');
 					return row[header]
 						.split('; ')
 						.map(v=>{
